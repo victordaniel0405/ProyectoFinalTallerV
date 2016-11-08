fe0821bc38666d3a4b3130def9047f2ada43930c
Victor Muñoz <vdmunozj@unal.edu.co>
2016-11-07 20:31:59 -0500

    Se crea el modulo PWM pero no se comfigura ya que se tienen dudas si se puede alcanzar el periodo adecuado para los servomotores

---
+++ ProyectoFinalTallerV.X/ModuloPWM.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   ModuloPWM.h
+ * Author: Victor Muñoz
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef MODULO_PWM_H
+#define	MODULO_PWM_H
+
+#include <xc.h> 
+
+void configuracionPWM(void);
+
+#endif	/* MODULO_PWM_H */
+
