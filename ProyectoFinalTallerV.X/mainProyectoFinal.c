5f8b9e554437bd42c0802fc6558990d5fe8bea02
Victor Muñoz <vdmunozj@unal.edu.co>
2016-11-07 20:34:39 -0500

    Se trata de configurar el pwm por el modulo ccp y por TMR1

--- ProyectoFinalTallerV.X/mainProyectoFinal.c
+++ ProyectoFinalTallerV.X/mainProyectoFinal.c
@@ -7,22 +7,35 @@
 
 #include <xc.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 #include "HAL.h"
 #include "ConfigurationBits.h"
 #include "ModuloMux.h"
 #include "ModuloTMR0.h"
+#include "ModuloTMR1.h"
 #include "ModuloADC.h"
+#include "ModuloPWM.h"
+#include "ModuloSerial.h"
+
+char bufferTransmision[64];
 
 short sensorsData[5];
 char iSensor;
 
 void main(void) {
 
+    // Configuramos el pin RC2 como un "pwm" controlado por TMR1
+    TRISCbits.RC2 = 0;  // Se configura el pin como salida
+    LATCbits.LC2 = 1;   // Se inicia el pin apagado
+    
     iSensor = 0; // Se inicia el valor del indice del vector sensorsData en 0
     
     configuracionMux();
     configuracionTMR0();
+    configuracionTMR1();
     configuracionADC();
+    configuracionPuertoSerial();    // Solo se encuentra activa la transmisión
 
     RCONbits.IPEN = 1; // Activamos el modelo de Prioridades en las interrupciones
     INTCONbits.PEIE = 1; // Activamos las interrupciones generadas por los perifericos
@@ -37,6 +50,8 @@ void main(void) {
             on0 = false; 
         }
         if (tareaADC){
+            sprintf(bufferTransmision,"Dato Sensor #%u = %u \r\n",iSensor+1,adcValue);
+            //sendStringChar(bufferTransmision);
             sensorsData[iSensor] = adcValue;            // Se guarda el valor en el vector
             iSensor = (iSensor < 4) ? iSensor + 1 : 0;  // Se aumenta el contador
             
@@ -55,6 +70,7 @@ void interrupt low_priority ISR_bajaPrioridad(void) {
 
 void interrupt high_priority ISR_altaPrioridad(void) {
     /* Aca van TODO el manejo de las intr de alta prioridad */
+    isrTMR1();
     isrADC();
     isrTMR0();
 }
