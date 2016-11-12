a27e9ce9ef526c35d6f3a8fffffac26ce7ccda19
Victor Muñoz <vdmunozj@unal.edu.co>
2016-11-11 19:48:30 -0500

    Se configura el TMR0 para llevar el control del PWM

--- ProyectoFinalTallerV.X/ModuloTMR0.c
+++ ProyectoFinalTallerV.X/ModuloTMR0.c
@@ -11,22 +11,42 @@
 #include "HAL.h"
 #include "ModuloTMR0.h"
 
-extern char iSensor;
+
+extern char tiempoAlto;
 
 void configuracionTMR0(void) {
-    // Configuracion TMR0
     on0 = false; // Variable para el TMR0
-
+    contadorPWM = 0; // Se inicia el contador del PWM
+    
+    // Configuración del pin PWM
+    TRISBbits.TRISB0 = 0; // Se configura el pin como salida
+    LATBbits.LATB0 = 1; // Se inicia el pin encendido
+    
+    // Configuracion TMR0
     T0CONbits.T08BIT = 1; // Se configura el TMR0 de 8 bits
-    T0CONbits.T0CS = 0;  // Se asigna la señal principal : Fcy
-    T0CONbits.PSA = 0;   // Se asigna el preescaler del TMRO
-    T0CONbits.T0PS = 6;  // Se establece el preescaler en 1:128
+    T0CONbits.T0CS = 0; // Se asigna la señal principal : Fcy
+    T0CONbits.PSA = 0; // Se asigna el preescaler del TMRO
+    T0CONbits.T0PS = 6; // Se establece el preescaler en 1:128
 
     INTCONbits.TMR0IF = 0; // Se baja la bandera del TMR0
     INTCON2bits.TMR0IP = 1; // Prioridad alta para el TMR0
     INTCONbits.TMR0IE = 1; // Se activa la interrucion debida al overflow del TMRO  
 
     TMR0 = 252; // Semilla para  0.1 milisegundos
+}
+
+void tareaTMR0(void) {
+    if (on0) {
+        contadorPWM++;
+        if (contadorPWM == tiempoAlto) {    // Se completa el ciclo de trabajo
+            LATBbits.LATB0 = 0;
+        }
+        if (contadorPWM == 100) {   // Se completa el periodo
+            LATBbits.LATB0 = 1;
+            contadorPWM = 0;
+        }
+        on0 = false;
+    }
 }
 
 void isrTMR0(void) {
@@ -37,15 +57,3 @@ void isrTMR0(void) {
         return;
     }
 }
-
-/*
-void encendidoTMR0(void) {
-
-}
- */
-
-/*
-void iniciarLedEstado(void) {
-    TRISCbits.RC4 = 0; // Se define el pin del Led
-    LED_ESTADO = 0; // Se inicia el con el led apagado
-}*/
